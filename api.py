5fe4eef4ae3e31190146584700eadd164b587b78
Ibad Soleja <152664643+ibadsoleja@users.noreply.github.com>
2023-12-05 00:30:05 +0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,16 @@
+###IMPORTS###
+import shopify
+from creds import*
+
+###API ACTIVATION###
+api_session = shopify.Session(merchant, '2023-01', token)
+shopify.ShopifyResource.activate_session(api_session)
+
+###GET PRODUCT DETAIL METHOD###
+def get_product_detail():
+    products = shopify.Product.find(limit=100)
+    for product in products:
+        print(f"Product: {product.title}")
+        for variant in product.variants:
+            print(f"Price: {variant.price}")
+            print(f"Availability: {variant.inventory_quantity}")
